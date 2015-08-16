12574fe8ad69a6f40159dabaf0af0566f5ece5b9
Fabian Rios <rocaboca@gmail.com>
2015-08-16 19:57:13 +0200

    Test api

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,4 +1,32 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Aeson
+import Data.Foldable (foldMap)
+import GHC.Generics
+import Text.Blaze.Html
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Blaze
+
+type API = "/" :> Raw
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = serveDirectory "resources"
+
+app :: Application
+app = serve api server
+
 main :: IO ()
-main = return ()
+main = do
+  putStrLn "Running on 8080 "
+  run 8080 app
