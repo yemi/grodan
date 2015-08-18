0012655a5bca3efd03d14d7fdb28fc09625f3227
Fabian <rocaboca@gmail.com>
2015-08-18 08:35:03 +0200

    Use blaze for page template

--- backend/Main.hs
+++ backend/Main.hs
@@ -9,24 +9,33 @@ module Main where
 import Data.Aeson
 import Data.Foldable (foldMap)
 import GHC.Generics
-import Text.Blaze.Html
+import Text.Blaze.Html5 hiding (main)
+import qualified Text.Blaze.Html5 as H
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.HTML.Blaze
 
-type API = "/" :> Raw
+data Page = Page
+
+instance ToMarkup Page where
+  toMarkup _ = 
+    H.docTypeHtml $ do 
+      H.body $ do
+        H.h1 "Hello world!"
+
+type API = "test" :> Get '[HTML] Page
 
 api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = serveDirectory "resources"
+server = return Page
 
 app :: Application
 app = serve api server
 
-main :: IO ()
+--main :: IO ()
 main = do
-  putStrLn "Running on 8080 "
-  run 8080 app
+  putStrLn "Running on 8081 "
+  run 8081 app
